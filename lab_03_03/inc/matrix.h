53ac46d5305a17c55407ae8799df76dd825929f6
pdv23u605 <pdv23u605@student.bmstu.ru>
2024-10-15 23:28:31 +0300

    add functions for matrices

--- lab_03_03/inc/matrix.h
+++ lab_03_03/inc/matrix.h
@@ -25,7 +25,46 @@ typedef enum
 {
     READ_SIZE_ERROR = 1,
     DATA_ALLOC_ERROR,
+    ELEM_READ_ERROR,
+    ROW_READ_ERROR,
+    COL_READ_ERROR,
 
 } matrix_errors_t;
+
+// Выделение памяти под стандартную матрицу
+int matrix_alloc(matrix_t *matrix);
+
+// Очищение памяти для стандартной матрицы
+void matrix_free(matrix_t *matrix);
+
+//Выделение памяти под разрееженную матрицу
+int sparse_matrix_alloc(sparse_matrix_t *sparse_matrix);
+
+// Очищение памяти для разреженной матрицы
+void sparse_matrix_free(sparse_matrix_t *sparse_matrix);
+
+// Чтение размера матриц
+void read_matrix_size(size_t *rows, size_t *cols);
+
+// Чтение матрицы, путем поэлементного ввода
+int read_matrix(matrix_t *matrix, size_t *num_non_zeros);
+
+// Заполнение матрицы, путем ввода координатным способом
+void feel_matrix_with_coords(matrix_t *matrix, size_t *num_non_zeros);
+
+// Заполнение матрицы рандомными элементами в рандомные позиции, соответствующий кол-ву элементов
+void fill_matrix_with_rand_elems(matrix_t *matrix, size_t *num_non_zeros);
+
+// Перевод из стандартной матрицы в разреженную
+void std_matrix_to_sparse(matrix_t matrix, sparse_matrix_t *sparse_matrix);
+
+// Перевод из разреженной в стандартную:
+void sparse_matrix_to_std(sparse_matrix_t sparse_matrix, matrix_t *matrix);
+
+// Вывод матрицы в стандартном виде
+void print_matrix(matrix_t matrix);
+
+// Вывод матрицы в разреженном виде
+void print_sparse_matrix(sparse_matrix_t sparse_matrix);
 
 #endif //LAB_03_03_SPARSE_MATRIX_H
--- lab_03_03/src/matrix.c
+++ lab_03_03/src/matrix.c
@@ -1,13 +1,17 @@
 #include "matrix.h"
 #include <stdio.h>
-
+#include <time.h>
 
 // Функции выделения и освобождения памяти
 void matrix_free(matrix_t *matrix)
 {
     for (size_t i = 0; i < matrix->rows; i++)
+    {
         free(matrix->data[i]);
+        matrix->data[i] = NULL;
+    }
     free(matrix->data);
+    matrix->data = NULL;
 }
 
 int matrix_alloc(matrix_t *matrix)
@@ -19,10 +23,7 @@ int matrix_alloc(matrix_t *matrix)
     {
         matrix->data[i] = calloc(matrix->cols, sizeof(int));
         if (!matrix->data[i])
-        {
-            matrix_free(matrix);
             return DATA_ALLOC_ERROR;
-        }
     }
     return EXIT_SUCCESS;
 }
@@ -32,10 +33,14 @@ void sparse_matrix_free(sparse_matrix_t *sparse_matrix)
     free(sparse_matrix->a);
     free(sparse_matrix->ia);
     free(sparse_matrix->ja);
+    sparse_matrix->a = NULL;
+    sparse_matrix->ja = NULL;
+    sparse_matrix->ia = NULL;
 }
 
 int sparse_matrix_alloc(sparse_matrix_t *sparse_matrix)
 {
+
     sparse_matrix->a = calloc(sparse_matrix->num_non_zeros, sizeof(int));
     if (!sparse_matrix->a)
         return DATA_ALLOC_ERROR;
@@ -52,11 +57,11 @@ int sparse_matrix_alloc(sparse_matrix_t *sparse_matrix)
 }
 
 // Функии чтения данных
-void read_matrix_size(size_t *rows, size_t *cols, size_t *num_non_zeros)
+void read_matrix_size(size_t *rows, size_t *cols)
 {
     int rc = EXIT_SUCCESS;
 
-    size_t tmp_cols, tmp_rows, tmp_non_zeros;
+    size_t tmp_cols, tmp_rows;
     do
     {
         printf("Введите количество строк матрицы: ");
@@ -83,6 +88,72 @@ void read_matrix_size(size_t *rows, size_t *cols, size_t *num_non_zeros)
     } while (rc != EXIT_SUCCESS);
     *cols = tmp_cols;
 
+
+}
+
+int read_matrix(matrix_t *matrix, size_t *num_non_zeros)
+{
+    printf("Введите матрицу размерностью %zu на %zu:\n", matrix->rows, matrix->cols);
+    for (size_t i = 0; i < matrix->rows; i++)
+        for (size_t j = 0; j < matrix->cols; j++)
+        {
+            if (scanf("%d", &matrix->data[i][j]) != 1)
+                return ELEM_READ_ERROR;
+            if (matrix->data[i][j] != 0)
+                (*num_non_zeros)++;
+        }
+    return EXIT_SUCCESS;
+}
+
+static void read_matrix_element(size_t *row, size_t *col, int *value, size_t rows, size_t cols)
+{
+    size_t tmp_row, tmp_col;
+    int tmp_value;
+    int rc = EXIT_SUCCESS;
+    do
+    {
+        printf("Введите индекс строки матрицы (от 0 до %zu): ", rows);
+        if (scanf("%zu", &tmp_row) != 1 || tmp_row < 0 || tmp_row > *row - 1)
+        {
+            printf("Ошибка ввода индекса строки.\n");
+            rc = ROW_READ_ERROR;
+            continue;
+        }
+        rc = EXIT_SUCCESS;
+    } while (rc == EXIT_SUCCESS);
+    *row = tmp_row;
+
+    do
+    {
+        printf("Введите индекс столбца матрицы (от 0 до %zu): ", rows);
+        if (scanf("%zu", &tmp_col) != 1 || tmp_col < 0 || tmp_col > *col - 1)
+        {
+            printf("Ошибка ввода индекса столбца.\n");
+            rc = COL_READ_ERROR;
+            continue;
+        }
+        rc = EXIT_SUCCESS;
+    } while (rc == EXIT_SUCCESS);
+    *col = tmp_col;
+
+    do
+    {
+        printf("Введите значение элемента матрицы (от 0 до %zu): ", rows);
+        if (scanf("%d", &tmp_value) != 1)
+        {
+            printf("Ошибка ввода значения элемента.\n");
+            rc = ELEM_READ_ERROR;
+            continue;
+        }
+        rc = EXIT_SUCCESS;
+    } while (rc == EXIT_SUCCESS);
+    *value = tmp_value;
+}
+
+void feel_matrix_with_coords(matrix_t *matrix, size_t *num_non_zeros)
+{
+    size_t tmp_non_zeros;
+    int rc = EXIT_SUCCESS;
     do
     {
         printf("Введите количество ненулевых элементов матрицы: ");
@@ -95,33 +166,100 @@ void read_matrix_size(size_t *rows, size_t *cols, size_t *num_non_zeros)
         rc = EXIT_SUCCESS;
     } while (rc != EXIT_SUCCESS);
     *num_non_zeros = tmp_non_zeros;
+    size_t row, col;
+    int value;
+    for (size_t i = 0; i < *num_non_zeros; i++)
+    {
+        read_matrix_element(&row, &col, &value, matrix->rows, matrix->cols);
+        matrix->data[row][col] = value;
+    }
 }
 
-int read_matrix()
+void fill_matrix_with_rand_elems(matrix_t *matrix, size_t *num_non_zeros)
 {
-    return 0;
+    srand(time(NULL));
+    for (size_t k = 0; k < *num_non_zeros; k++)
+    {
+        size_t i = rand() % matrix->rows;
+        size_t j = rand() % matrix->cols - 1;
+        if (matrix->data[i][j] == 0)
+            matrix->data[i][j] = rand() % 50 + 1;
+        else
+            k--;
+    }
 }
 
-int fill_matrix()
+
+// Функции перевода из одной формы в другую
+void std_matrix_to_sparse(matrix_t matrix, sparse_matrix_t *sparse_matrix)
 {
-    return 0;
+    sparse_matrix->rows = matrix.rows;
+    sparse_matrix->cols = matrix.cols;
+    for (size_t i = 0; i < matrix.rows; i++)
+    {
+        sparse_matrix->ia[i] = sparse_matrix->num_non_zeros;
+        for (size_t j = 0; j < matrix.cols; j++)
+            if (matrix.data[i][j] != 0)
+            {
+                sparse_matrix->a[sparse_matrix->num_non_zeros] = matrix.data[i][j];
+                sparse_matrix->ja[sparse_matrix->num_non_zeros] = j;
+                sparse_matrix->num_non_zeros++;
+            }
+    }
+    sparse_matrix->ia[sparse_matrix->rows] = sparse_matrix->num_non_zeros;
 }
 
-void sparse_matrix_to_std()
+void sparse_matrix_to_std(sparse_matrix_t sparse_matrix, matrix_t *matrix)
 {
-    return;
+    for (size_t i = 0; i < sparse_matrix.rows; i++)
+        for (size_t j = 0; j < sparse_matrix.cols; j++)
+            matrix->data[i][j] = 0;
+
+    for (size_t row = 0; row < sparse_matrix.rows; row++)
+        for (size_t ind = sparse_matrix.ia[row]; ind < sparse_matrix.ia[row + 1]; ind++)
+            matrix->data[row][sparse_matrix.ja[ind]] = sparse_matrix.a[ind];
 }
 
-void std_matrix_to_sparse()
+
+// Функции вывода матриц
+void print_matrix(matrix_t matrix)
 {
-    return;
+    for (size_t i = 0; i < matrix.rows; i++)
+        for (size_t j = 0; j < matrix.cols; j++)
+        {
+            if (j == matrix.cols - 1)
+                printf("%d\n", matrix.data[i][j]);
+            else
+                printf("%d ", matrix.data[i][j]);
+        }
 }
 
-void print_matrix()
+void print_sparse_matrix(sparse_matrix_t sparse_matrix)
 {
-}
+    printf("Вектор А, который содержит значения ненулевых элементов:\n");
+    for (size_t i = 0; i < sparse_matrix.num_non_zeros; i++)
+    {
+        if (i == sparse_matrix.num_non_zeros - 1)
+            printf("%d\n", sparse_matrix.a[i]);
+        else
+            printf("%d ", sparse_matrix.a[i]);
+    }
 
-void print_sparse_matrix()
-{
-    return;
+    printf("Вектор JA, который содержит номера столбцов, для элеметнов вектора А:\n");
+    for (size_t i = 0; i < sparse_matrix.num_non_zeros; i++)
+    {
+        if (i == sparse_matrix.num_non_zeros - 1)
+            printf("%zu\n", sparse_matrix.ja[i]);
+        else
+            printf("%zu ", sparse_matrix.ja[i]);
+    }
+
+    printf("Вектор IA, который содержит индекс вектора JA, с которой начинается строка матрицы A:\n");
+    for (size_t i = 0; i < sparse_matrix.rows + 1; i++)
+    {
+        if (i == sparse_matrix.rows)
+            printf("%zu\n", sparse_matrix.ia[i]);
+        else
+            printf("%zu ", sparse_matrix.ia[i]);
+    }
 }
